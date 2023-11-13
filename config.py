9a60c309d51ab76d301f25102f411223a5417357
Rushikesh Kundkar <r4002001005025k@gmail.com>
2023-11-13 23:16:44 +0530

    PYS-7 : seperated config and main files

---
+++ config.py
@@ -0,0 +1,14 @@
+# colors
+white = (255,255,255)
+red = (255,0,0)
+black = (0,0,0)
+blue = (0,0,255)
+green = (0,255,0)
+rb = (255,0,128)
+
+# screen size variables
+width = 1200
+height = 660
+
+# music
+music = False
